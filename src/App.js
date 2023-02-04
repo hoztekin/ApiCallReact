e28d432f8c0d9cadd7300848102dcee92dfe08f7
hoztekin <hoztekin81@hotmail.com>
2023-02-05 01:09:28 +0300

    Finally

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import './App.css';
-import SearcHeader from './SearchHeader';
-import searchImages from './api'
+import "./App.css";
+import SearcHeader from "./SearchHeader";
+import searchImages from "./api";
+import { useState } from "react";
+import ImageList from "./components/ImageList";
 
 function App() {
-  
-
-  const handleSubmit = (term) => {
-    searchImages(term);
+  const [images, setImages] = useState([]);
+  const handleSubmit = async (term) => {
+    const result = await searchImages(term);
+    setImages(result);
   };
 
   return (
     <div className="App">
       <SearcHeader search={handleSubmit} />
+      <ImageList imagesPlaceHolder={images}/>
     </div>
   );
 }
--- src/SearchHeader.js
+++ src/SearchHeader.js
@@ -1,15 +1,23 @@
-
+import { useState } from "react";
 
 function SearcHeader({ search }) {
+const [valueInput, setValue] = useState('')
+
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    search('halil');
+    debugger;
+    search(valueInput);
   };
+  const handleChange = (event) =>{
+    setValue( event.target.value);
+  };
+ 
   return (
     <div className="SearchDiv">
       <form onSubmit={handleFormSubmit}>
         <label>Ne aramıştınız?</label>
-        <input   />
+        <input value={valueInput} onChange={handleChange}   />
+        
       </form>
     </div>
   );
